e06d7524c830271b17798c5bfcfbde052e6f3591
Nicolas Kempf <kempfnicolas@gmail.com>
2021-09-29 21:37:22 +0200

    MAJ DB

--- Calcul de la rentabilité d'un investissement.R
+++ Calcul de la rentabilité d'un investissement.R
@@ -6,8 +6,9 @@ library(tidyverse)
 library(openxlsx)
 
 # Import
-journal_db <- read.xlsx("input/NMK_db.xlsx",sheet = "Journal",startRow = 2,detectDates = T)
-
+journal_db <- read.xlsx("input/NMK_db.xlsx",sheet = "Journal",startRow = 2,detectDates = T) %>% 
+  mutate(timestamp = as.Date.character(timestamp,format = c("%d/%m/%Y")),
+         amount = as.numeric(amount))
 
 from ="2020-10-01"
 to ="2021-09-28"
@@ -18,10 +19,6 @@ adjustment <- TRUE # Toujours travailler en prix ajustés
 
 getSymbols(symbols, from=from, to=to, 
            adjust=TRUE) # Danone n'est pas reconnu
-TTE.adj <- TTE
-
-getSymbols(symbols, from=from, to=to, 
-           adjust=FALSE) # Danone n'est pas reconnu
 
 Cl(TTE["2021-09-28"])
 
@@ -29,12 +26,25 @@ day <- "2021-09-27"
 
 # Exemple : total
 total <- journal_db %>% 
-  filter(instrument == "Total") %>% 
+  filter(type == "Transaction" & instrument == "Total") %>% 
   mutate(
     pru = (amount * price + fees) / amount, # prix de revient unitaire
     Pt = as.numeric(Cl(TTE[day])),
     Pt_1 = as.numeric(Cl(TTE[timestamp])),
     Pt_adj = Pt*0.995, # ajusté des frais de Boursorama
     Pt_1_adj = Pt_1*1.005, # Ajusté des frais de Boursorama
-    HPR = (Pt_1 - Pt) / Pt
+    HPR = (Pt_1 - Pt) / Pt, # Holding Period Return
   )
+
+dividende <- journal_db %>% 
+  filter(type == "Dividendes" & instrument == "Total")
+
+class(total)
+
+total.xts <- as.xts(total %>% select(amount),order.by = total$timestamp)
+periodicity(total.xts)
+
+
+test <- apply.monthly(total.xts,FUN=sum)
+
+periodicity(total.xts)
